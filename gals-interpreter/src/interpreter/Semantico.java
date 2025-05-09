c21e21257a874084737a341b505f2e7c178fae19
Cauã Ribas <cauaribasone@gmail.com>
2025-05-09 20:15:48 -0300

    Initial commit

---
+++ gals-interpreter/src/interpreter/Semantico.java
@@ -0,0 +1,9 @@
+package interpreter;
+
+public class Semantico implements Constants
+{
+    public void executeAction(int action, Token token)	throws SemanticError
+    {
+        System.out.println("Ação #"+action+", Token: "+token);
+    }	
+}
