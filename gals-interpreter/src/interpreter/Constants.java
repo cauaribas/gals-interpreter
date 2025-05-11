4d4ae28051ea0c8bb83035dd8f637429d377303c
Cauã Ribas <cauaribasone@gmail.com>
2025-05-11 14:33:26 -0300

    fix: correctly classify log as token

--- gals-interpreter/src/interpreter/Constants.java
+++ gals-interpreter/src/interpreter/Constants.java
@@ -6,17 +6,16 @@ public interface Constants extends ScannerConstants, ParserConstants
     int DOLLAR   = 1;
 
     int t_SHOW = 2;
-    int t_VARIABLE = 3;
-    int t_NUMBER = 4;
-    int t_ADD = 5;
-    int t_SUBTRACT = 6;
-    int t_MULTIPLY = 7;
-    int t_DIVIDE = 8;
-    int t_EXPONENTIATION = 9;
-    int t_LOG = 10;
+    int t_LOG = 3;
+    int t_VARIABLE = 4;
+    int t_NUMBER = 5;
+    int t_ADD = 6;
+    int t_SUBTRACT = 7;
+    int t_MULTIPLY = 8;
+    int t_DIVIDE = 9;
+    int t_EXPONENTIATION = 10;
     int t_EQUAL = 11;
     int t_OPEN_PAREN = 12;
     int t_CLOSE_PAREN = 13;
     int t_EOL = 14;
-
 }
